de410a97fc4a180fd62f463282a205eb552c3fad
Fazeela <fazeelashams99@gmail.com>
2017-03-27 21:02:40 +0500

    Initial commit

---
+++ app/src/main/java/com/gnirt69/customlistviewadapterexam/contact.java
@@ -0,0 +1,44 @@
+package com.gnirt69.customlistviewadapterexam;
+
+
+public class contact {
+    private String name;
+    private String phone;
+    private String nickname;
+
+    //Constructor
+
+    public contact(int id,String name, String phone, String nickname) {
+        this.name = name;
+        this.phone = phone;
+        this.nickname = nickname;
+    }
+
+    //Setter, getter
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+
+}
